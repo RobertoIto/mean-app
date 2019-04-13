e10f3f4831b9f2af295f1cddd56551d32bffada0
Rito <rito@my.centennialcollege.ca>
2019-04-13 11:16:54 -0400

    Lesson 24
    Creating model of Interfaces to use as type.

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,5 +1,7 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 
+import { Post } from '../post.model';
+
 @Component({
   selector: 'app-post-create',
   templateUrl: './post-create.component.html',
@@ -9,7 +11,7 @@ import { Component, EventEmitter, Output } from '@angular/core';
 export class PostCreateComponent {
   enteredTitle = '';
   enteredContent = '';
-  @Output() postCreated = new EventEmitter();
+  @Output() postCreated = new EventEmitter<Post>();
 
   // newPost = '';
 
@@ -22,7 +24,7 @@ export class PostCreateComponent {
   onAddPost() {
     // this.newPost = this.enteredValue;
 
-    const post = {
+    const post: Post = {
       title: this.enteredTitle,
       content: this.enteredContent
     };
